b1bf68a227efd46ecfbbdadb7a612a821b11a66f
vedanshipanchal <vedanshipanchal@gmail.com>
2024-10-18 17:40:04 +0100

    Calculator

---
+++ main.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enternumber1:"))
+num2 = int(input("Enter number 2:"))
+
+print("select options:")
+print("a.add")
+print("b.sub")
+print("c.mul")
+print("d.div")
+
+def add(num1,num2):
+    return num1+num2
+print(add(num1,num2))
+def sub(num1,num2):
+    return num1-num2
