e051253b772f520bbe4fcf2249eddef662066e50
vedanshipanchal <vedanshipanchal@gmail.com>
2024-10-18 18:05:29 +0100

    Calculator

--- main.py
+++ main.py
@@ -5,10 +5,24 @@ print("select options:")
 print("a.add")
 print("b.sub")
 print("c.mul")
-print("d.div")
+option = input("Enter a or b or c or d:")
+
+
 
 def add(num1,num2):
     return num1+num2
-print(add(num1,num2))
+print(add(num1,num2)) 
+
 def sub(num1,num2):
     return num1-num2
+print(add(num1,num2)) 
+
+def mul(num1,num2):
+    return num1*num2
+print(mul(num1,num2)) 
+
+def div(num1,num2):
+    return num1/num2
+print(div(num1,num2)) 
+
+
